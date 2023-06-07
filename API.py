2b268444c69646bc7cafb8dda0bf3285cb577dc3
JerronAB <92005206+JerronAB@users.noreply.github.com>
2023-06-07 12:37:14 -0500

    Breaking .concat method temporarily

--- API.py
+++ API.py
@@ -12,13 +12,13 @@
 #next steps are working on construct and concat
 
 class basicDataTypes: #once things get more complex, this class will be used by CLData to represent columns
-    def __init__(self,name='dataType',aliasList=[],validator=None,dataMod=None) -> None:
+    def __init__(self,name='dataType',aliasList=[],validator: callable =None,dataMod: callable =None):
         self.name = name
         self.aliasList = aliasList
         self.validator = validator
         self.dataMod = dataMod
     def validateData(self,data):
-        if self.validator(data) is not True: raise Exception()
+        return self.validator(data) # do we also want to raise Exception()
     def aliasMatch(self,alias):
         return alias.lower() in self.aliasList
     def modData(self,data):
@@ -39,22 +39,26 @@ class CLData:
             'section': basicDataTypes('section',['section', 'section name', 'sectionid']),
             'instructor': basicDataTypes('instructor',['instructor', 'instructor name']),
             'instructor-email': basicDataTypes('instructor-email',['email', 'email address', 'instructor email','email id']),
-            'start-date': basicDataTypes('start-date',['first day', 'start date', 'commencement date', 'startdate']),
-            'end-date': basicDataTypes('end-date',['last day', 'end date', 'enddate', 'finish date', 'completion date']),
+            'start-date': basicDataTypes('start-date',['first day', 'start date', 'commencement date', 'startdate', 'begin date']),
+            'end-date': basicDataTypes('end-date',['last day', 'end date', 'enddate', 'finish date', 'completion date', 'end date']),
             'credits': basicDataTypes('credits',['credit hours', 'course units', 'units']),
             'delivery-mode':basicDataTypes('delivery-mode',['delivery mode','mode','delivery'])
         }
-    def construct(self,*columns):
+    def construct(self,*columns: str):
         self.constructed = []
         if not all([column.lower() in self.dataAliases for column in columns]): raise Exception('One or more columns in the given list is not present in the basicDataType dictionary.')
         self.constructed = [dataType for column in columns for key,dataType in self.dataAliases if dataType.aliasMatch(column)] #so this basically builds a list of our basicDataType objects, in order based on user input
         #next we move the data to match this constructed list
-    def concat(self,new_column_name,*columns): #takes basic data types and allows you to concatenate them into a new column; CLData.concat("sectionID","term","course","section")
+    def concat(self,new_column_name: str,*columns: str): #takes basic data types and allows you to concatenate them into a new column; CLData.concat("sectionID","term","course","section")
         self.syncData('vertical')  #needs data synced vertically
         self.dataAliases[new_column_name] = basicDataTypes(name=new_column_name)
         self.DataVertical[new_column_name] = [] #[cell for col_name in columns for cell in self.DataVertical[col_name]]
         indices = [self.ColumnsHorizontal.index(column) for column in columns]
-        self.DataVertical[new_column_name].append(row[index] for index in indices for row in self.DataHorizontal) #no clue if this'll work or not; will definitely need to troubleshoot
+        #lambda indexList: ouput += row[index] for index in indexList
+        '''output = ''
+        appendIndices = (row[index] for row in self.DataHorizontal for index in indexList)
+        output +='''
+        self.DataVertical[new_column_name].append([row[index] for index in indices for row in self.DataHorizontal]) #no clue if this'll work or not; will definitely need to troubleshoot
         self.syncData('horizontal') #resync the data horizontally
     def associate(self): #this should essentially rename columns if they match an alias above; GOT TO SHAPE THIS UP, it's just ugly
         for index,columnName in enumerate(self.ColumnsHorizontal):
@@ -66,9 +70,9 @@ class CLData:
         self.DataHorizontal = tuple([item for item in data])
         self.lastSync = 'horizontal'
         self.integrityCheck()
-    def addRow(self, line):
+    def addRow(self, line: list):
         self.Data += (line,)
-    def addRows(self,rows):
+    def addRows(self,rows: list):
         for row in rows: self.addRow(row)
         self.integrityCheck()
     def syncToVert(self):
@@ -82,23 +86,23 @@ class CLData:
         #self.DataHorizontal = [data[index] for index in range(len(data))]
         self.integrityCheck()
         self.lastSync = 'horizontal'
-    def syncData(self, syncTo):
+    def syncData(self, syncTo: str):
         if syncTo == 'vertical': self.syncToVert()
         elif syncTo == 'horizontal': self.syncToHor()
         elif syncTo == 'auto' and self.lastSync == 'vertical': self.syncToHor() #automatically determines what to sync based on last synced item
         elif syncTo == 'auto' and self.lastSync == 'horizontal': self.syncToVert()
-    def mapRows(self, mappedFunction, inPlace=False): #here, I need to explore using lambda & map, vs. using eval(), vs. using exec()
+    def mapRows(self, mappedFunction: callable, inPlace: bool =False): #here, I need to explore using lambda & map, vs. using eval(), vs. using exec()
         print(f'Function being passed: {mappedFunction}')
         print(f'Modifying in-place: {inPlace}')
         if inPlace is True: self.Data = [mappedFunction(row) for row in self.DataHorizontal if row is not None]
         else: return [mappedFunction(row) for row in self.Data if row is not None]
-    def deDup(self, dedupColumn): #make this nicer and cleaner
+    def deDup(self, dedupColumn: str): #make this nicer and cleaner
         index = self.ColumnsHorizontal.index(dedupColumn)
         dedupping_set = set()
         dedupped_list = [item for item in self.Data if item[index] not in dedupping_set and not dedupping_set.add(item[index])] #set() does not allow duplicate values. Here we add all items to a set on each loop, and stop loop if item is in set already
         self.Data = ()
         self.setDataHorizontal(dedupped_list)
-    def export(self,filename): #looking at export function to make sure it doesn't export empty cells
+    def export(self,filename: str): #looking at export function to make sure it doesn't export empty cells
         from csv import writer
         #this uses the 'writer' function from the csv module
         nonetoString = lambda cells: [str(cell or '') for cell in cells]
@@ -108,14 +112,14 @@ class CLData:
             my_writer.writerow(nonetoString(self.ColumnsHorizontal))
             for row in self.Data:
                 my_writer.writerow(nonetoString(row))
-    def CSVimport(self, filename):
+    def CSVimport(self, filename: str):
         from csv import reader
         with open(filename, 'r', encoding='ISO-8859-1') as csvfile: #UTF-8
                 self.filename = filename
                 newname = filename.split('\\')
                 self.name = newname[-1].replace('.csv','')
                 csvData = [row for row in reader(csvfile)]
-                self.ColumnsHorizontal = list(map(lambda input_str: input_str.replace("ï»¿",""), csvData.pop(0)))
+                self.ColumnsHorizontal = [(lambda inputStr: inputStr.replace("ï»¿",""))(colName) for colName in csvData.pop(0)]
                 self.setDataHorizontal(csvData)
     def integrityCheck(self): #this will have to check both vertical and horizontal types now
         print(f'Running integrity check---')
@@ -132,6 +136,6 @@ class courses(CLData): #here I need to flesh out a minor example of a subclass t
 
 organizational_units = ['OrgUnitIdentifier', 'Name', 'Acronym', 'ParentIdentifier', 'Type']
 academic_term = ['TermIdentifier', 'Name', 'BeginDate', 'EndDate', 'ParentIdentifier', 'Type']
-courses = ['CourseIdentifier', 'Subject', 'Number', 'Title', 'Credits', 'OrgUnitIdentifier', 'Type', 'Description', 'CIPCode']
+courses_ = ['CourseIdentifier', 'Subject', 'Number', 'Title', 'Credits', 'OrgUnitIdentifier', 'Type', 'Description', 'CIPCode']
 sections = ['SectionIdentifier', 'TermIdentifier', 'CourseIdentifier', 'Subject', 'CourseNumber', 'Number', 'BeginDate', 'EndDate', 'OrgUnitIdentifier', 'Title', 'Credits', 'DeliveryMode', 'Location', 'Description', 'CrossListingIdentifier']
 instructors = ['PersonIdentifier', 'SectionIdentifier', 'FirstName', 'LastName', 'Email', 'Role']
--- CSV Import.py
+++ CSV Import.py
@@ -1,23 +1,10 @@
-import API
-
-try:
-    envDict = {}
-    with open('.env') as envFile:
-        for line in envFile:
-            if line[0] != "#": key, value = line.strip().split('=')
-            envDict[key.strip()] = value.strip()
-except:
-    print('.env file was not found, but is required for this script. Is your Python session running in the correct directory?\n')
-    exit()
-
-[print(f'{key}: {envDict[key]}') for key in envDict]
-
-PS_Export = API.courses()
+'''PS_Export = API.courses()
 PS_Export = API.CLData() #not sure how/where we want these classes to be instantiated/placed
-PS_Export.CSVimport('PS_Class_Table.csv')
+PS_Export.CSVimport('PS_Class_Table.csv')'''
 
 import API
 PS_Export = API.CLData()
 PS_Export.CSVimport('class_table.csv')
 PS_Export.associate()
 PS_Export.concat('sectionID','term','subject','catalog','section')
+PS_Export.DataVertical
