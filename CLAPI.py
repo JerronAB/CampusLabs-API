9029bc5fa1394b762336e45e5cadd997bdeb457f
JerronAB <92005206+JerronAB@users.noreply.github.com>
2023-06-22 12:50:51 -0500

    Moving library name. Changed structure of .associate() and .insertDataType methods

--- CLAPI.py
+++ CLAPI.py
@@ -8,19 +8,25 @@ class basicDataTypes: #once things get more complex, this class will be used by 
         self.name = name
         self.aliasList = aliasList
         self.validator = validator
-        self.dataMod = dataMod
+        self.dataModder = dataMod
     def __eq__(self,string):
         return self.__contains__(string)
     def __contains__(self,string):
         if string.lower() in [item.lower() for item in self.aliasList]: return True
+        if string.lower() == self.name: return True
         else: return False
     def validateData(self,data):
         return self.validator(data) # do we also want to raise Exception()
     def aliasMatch(self,alias):
         return alias.lower() in self.aliasList
-    def modData(self,data):
-        lambda data: data.strip() #just throwing this in anyway
-        data = self.dataMod(data)
+    def modData(self,list_of_data):
+        stripper = lambda input: input.strip() #just throwing this in anyway
+        data = [stripper(cell) for cell in list_of_data]
+        try:
+            data = [self.dataModder(cell) for cell in data]
+            return data
+        except:
+            return data
 
 class CLData: 
     def __init__(self) -> None:
@@ -50,11 +56,26 @@ class CLData:
         print('Complete... syncing data back to horizontal attributes. ')
         self.syncData('horizontal') #resync the data horizontally
 
-    def associate(self): 
-        #this should essentially rename columns if they match an alias above; GOT TO SHAPE THIS UP, it's just ugly
+    def associate(self): #this whole stanza is awful
+        #this should essentially rename columns if they match an alias above
+        self.syncData('auto')
         for index,columnName in enumerate(self.ColumnsHorizontal):
             for key,datatype in self.dataAliases.items():
                 if datatype.aliasMatch(columnName): self.ColumnsHorizontal[index] = datatype.name
+        self.syncData('vertical')
+        for key,data in self.DataVertical.items(): 
+            for name,datatype in self.dataAliases.items():
+                if key in datatype: #this is possible because of the __contains__ attribute on basicDataTypes
+                    self.DataVertical[key] = datatype.modData(data)
+        keys = list(self.DataVertical.keys())
+        key = keys[0]
+        vertLength = len(self.DataVertical[key])              
+        for basicDataType in self.dataAliases.values():
+            if basicDataType == 'INSERTION':
+                print(f'During association, an insertable datatype, {basicDataType.name} was detected.')
+                self.DataVertical[basicDataType.name] = ['' for i in range(vertLength)]
+                self.DataVertical[basicDataType.name] = basicDataType.modData(self.DataVertical[basicDataType.name])
+        self.syncData('horizontal')
 
     def setDataHorizontal(self, data):
         print(f'setData running on iterable with {len(data)} items.')
@@ -77,16 +98,17 @@ class CLData:
         elif syncTo == 'auto' and self.lastSync == 'horizontal': self.syncToVert()
 
     def syncToVert(self):
+        print('Syncing data to vertical dictionaries... ',end='')
         self.DataVertical = {columnName: [row[index] for row in self.DataHorizontal] for index,columnName in enumerate(self.ColumnsHorizontal)}
         self.lastSync = 'vertical'
+        print('Done.')
 
     def syncToHor(self):
-        print('Performing syncToHor()')
+        print('Syncing data to horizontal lists... ',end='')
         self.ColumnsHorizontal.clear()
         self.ColumnsHorizontal = [columnName for columnName in self.DataVertical.keys()]
         name, data = next(iter(self.DataVertical.items())) #accesses first value in dictionary
         length = len(data)
-        print(f'Length of data: {length}')
         self.DataHorizontal = []
         for index in range(length):
             self.DataHorizontal.append([data[index] for columnName,data in self.DataVertical.items()]) #generating a list of lists
@@ -95,25 +117,25 @@ class CLData:
 
     def addDataType(self, name: str, aliases: list, validator=None, datamodifier=None):
         self.dataAliases[name] = basicDataTypes(name,aliases,validator,datamodifier)
+    def insertDataType(self, name: str, validator=None, datamodifier=None): #inserts data if none exists
+        self.addDataType(name, ['INSERTION'], validator, datamodifier)
 
-    def mapRows(self, mappedFunction: callable, inPlace: bool =False): 
-        #here, I need to explore using lambda & map, vs. using eval(), vs. using exec()
-        print(f'Function being passed: {mappedFunction}')
-        print(f'Modifying in-place: {inPlace}')
-        if inPlace is True: self.Data = [mappedFunction(row) for row in self.DataHorizontal if row is not None]
-        else: return [mappedFunction(row) for row in self.Data if row is not None]
+    def mapRows(self, mappedFunction: callable): 
+        self.Data = [mappedFunction(row) for row in self.DataHorizontal if row is not None]
+        return [mappedFunction(row) for row in self.Data if row is not None]
 
     def deDup(self, dedupColumn: str): #make this nicer and cleaner
+        self.syncData('horizontal')
         index = self.ColumnsHorizontal.index(dedupColumn)
         dedupping_set = set()
-        dedupped_list = [item for item in self.Data if item[index] not in dedupping_set and not dedupping_set.add(item[index])] #set() does not allow duplicate values. Here we add all items to a set on each loop, and stop loop if item is in set already
-        self.Data = ()
+        dedupped_list = [item for item in self.DataHorizontal if item[index] not in dedupping_set and not dedupping_set.add(item[index])] #set() does not allow duplicate values. Here we add all items to a set on each loop, and stop loop if item is in set already
         self.setDataHorizontal(dedupped_list)
+        self.syncData('vertical')
 
     def CSVexport(self,filename: str):
         from csv import writer
         nonetoString = lambda cells: [str(cell or '') for cell in cells]
-        print(f'Writing to... {filename}')
+        print(f'Writing to... {filename}\n')
         with open(filename,'w',newline='') as csv_file:
             my_writer = writer(csv_file, delimiter = ',')
             my_writer.writerow(nonetoString(self.ColumnsHorizontal))
@@ -121,6 +143,9 @@ class CLData:
 
     def CSVimport(self, filename: str):
         from csv import reader
+        self.ColumnsHorizontal = []
+        self.DataHorizontal = []
+        self.DataVertical = []
         with open(filename, 'r', encoding='ISO-8859-1') as csvfile: #UTF-8
                 self.filename = filename
                 newname = filename.split('\\')
@@ -130,7 +155,7 @@ class CLData:
                 self.setDataHorizontal(csvData)
 
     def integrityCheck(self): #this will have to check both vertical and horizontal types now
-        print(f'Running integrity check...')
+        print(f'Running integrity check... ',end='')
         if not all(isinstance(line, list) for line in self.DataHorizontal): raise TypeError("tableData data must be a tuple of lists.")
         if not all(len(line) == len(self.ColumnsHorizontal) for line in self.DataHorizontal): raise Exception("Items in self.DataHorizontal do not have the same length as self.ColumnsHorizontal")
         print('Passed.')
--- CSV Import.py
+++ CSV Import.py
@@ -1,47 +1,70 @@
-import API
+import CLAPI
 
-PS_Export = API.CLData()
-PS_Export.addDataType('term', ['term', 'period', 'time-period','time period'], lambda x: x.isnumeric() and len(x) == 4)
-PS_Export.addDataType('subject', ['subject', 'subj'], lambda x: len(x) < 4)
-PS_Export.addDataType('class-title', ['class-desc', 'class desc','class description', 'title', 'description'])
-PS_Export.addDataType('catalog', ['catalog', 'catalogue', 'course num', 'course nbr', 'number'])
-PS_Export.addDataType('section', ['section', 'section name','sectionid'])
-PS_Export.addDataType('instructor', ['instructor', 'instructor name'],lambda x: x.lower() != 'staff')
-PS_Export.addDataType('instructor-email',['email', 'email address', 'instructor email', 'email id'])
-PS_Export.addDataType('start-date', ['first day', 'start date','commencement date', 'startdate', 'begin date'])
-PS_Export.addDataType('end-date', ['last day', 'end date','enddate', 'finish date', 'completion date', 'end date'])
-PS_Export.addDataType('credits', ['credit hours', 'course units', 'units'])
-PS_Export.addDataType('delivery-mode', ['delivery mode', 'mode', 'delivery'],datamodifier=lambda x: x.replace("HB", "Hybrid").replace("BP", "Face2Face").replace("BW", "Online").replace("BL","Face2Face").replace("IB","Face2Face").replace("P","Face2Face")) #I know I could squash this into a list that I unpack, but this is fine
-PS_Export.addDataType('type',['type'],datamodifier=lambda x: 'Undergraduate')
-PS_Export.addDataType('org-unit',['orgunitidentifier','orgunit'],datamodifier=lambda x: '')
+Section_Export = CLAPI.CLData()
+Section_Export.addDataType('term', ['term', 'period', 'time-period','time period'], lambda x: x.isnumeric() and len(x) == 4)
+Section_Export.addDataType('subject', ['subject', 'subj'], lambda x: len(x) < 4)
+Section_Export.addDataType('class-title', ['class-desc', 'class desc', 'class description', 'title', 'description'])
+Section_Export.addDataType('catalog', ['catalog', 'catalogue', 'course num', 'course nbr', 'number'])
+Section_Export.addDataType('section', ['section', 'section name', 'sectionid'])
+Section_Export.addDataType('instructor', ['instructor', 'instructor name'], lambda x: x.lower() != 'staff')
+Section_Export.addDataType('instructor-email', ['email', 'email address', 'instructor email', 'email id'])
+Section_Export.addDataType('start-date', ['first day', 'start date', 'commencement date', 'startdate', 'begin date'])
+Section_Export.addDataType('end-date', ['last day', 'end date','enddate', 'finish date', 'completion date', 'end date'])
+Section_Export.addDataType('credits', ['credit hours', 'course units', 'units'])
+Section_Export.addDataType('delivery-mode', ['delivery mode', 'mode', 'delivery'], datamodifier=lambda x: x.replace("HB", "Hybrid").replace("BP", "Face2Face").replace("BW", "Online").replace("BL", "Face2Face").replace("IB", "Face2Face").replace("P", "Face2Face"))  # I know I could squash this into a list that I unpack, but this is fine
+Section_Export.insertDataType('type', datamodifier=lambda x: 'Undergraduate')
+Section_Export.insertDataType('org-unit', datamodifier=lambda x: '')
+Section_Export.insertDataType('location', datamodifier=lambda x: '')
+Section_Export.insertDataType('desc', datamodifier=lambda x: '')
+Section_Export.insertDataType('crosslisting', datamodifier=lambda x: '')
 
-PS_Export.CSVimport('2temp.class_table.csv')
-PS_Export.associate()
-PS_Export.concat('SectionIdentifier', 'term', 'subject', 'catalog', 'section')
-PS_Export.concat('CourseIdentifier', 'subject', 'catalog')
+# name-in-report:name-in-dataType
+construction_dictionary = {'SectionIdentifier': 'SectionIdentifier', 
+                           'TermIdentifier': 'term', 
+                           'CourseIdentifier': 'CourseIdentifier', 
+                           'Subject': 'subject', 
+                           'CourseNumber': 'catalog',
+                           'Number': 'section', 
+                           'BeginDate': 'start-date', 
+                           'EndDate': 'end-date', 
+                           'Title': 'class-title', 
+                           'Credits': 'credits', 
+                           'DeliveryMode': 'delivery-mode', 
+                           'OrgUnitIdentifier': 'org-unit', 
+                           'Type': 'type',
+                           'Location': 'location',
+                           'Description': 'desc',
+                           'CrossListingIdentifier': 'crosslisting'
+                           }
 
-PS_Export.prune()
+Section_Export.CSVimport('temp.class_table_4226.csv')
+Section_Export.associate()
+Section_Export.concat('SectionIdentifier', 'term', 'subject', 'catalog', 'section')
+Section_Export.concat('CourseIdentifier', 'subject', 'catalog')
+Section_Export.prune()
+Section_Export.constructReport(construction_dictionary)
+Section_Export.deDup('SectionIdentifier')
+Section_Export.CSVexport('../4226_sections_report_temp.csv')
 
-#PS_Export.constructReport('SectionIdentifier', 'TermIdentifier', 'CourseIdentifier', 'Subject', 'CourseNumber', 'Number', 'BeginDate', 'EndDate', 'OrgUnitIdentifier', 'Title', 'Credits', 'DeliveryMode', 'Location', 'Description', 'CrossListingIdentifier')
-PS_Export.constructReport({'SectionIdentifier':'SectionIdentifier', 'TermIdentifier':'term', 'CourseIdentifier':'CourseIdentifier', 'Subject':'subject', 'CourseNumber':'catalog', 'Number':'section', 'BeginDate':'start-date', 'EndDate':'end-date', 'Title':'class-title', 'Credits':'credits', 'DeliveryMode':'delivery-mode','OrgUnitIdentifier':'org-unit','Type':'type'})
-PS_Export.CSVexport('../report_temp.csv')
+Section_Export.CSVimport('temp.class_table_4224.csv')
+Section_Export.associate()
+Section_Export.concat('SectionIdentifier', 'term', 'subject', 'catalog', 'section') #in the future I want this to persist beyond a single import
+Section_Export.concat('CourseIdentifier', 'subject', 'catalog')
+Section_Export.prune()
+Section_Export.constructReport(construction_dictionary)
+Section_Export.deDup('SectionIdentifier')
+Section_Export.CSVexport('../4224_sections_report_temp.csv')
 
-#PS_Export.CSVimport('2temp.class_table.csv')
-#PS_Export.associate()
-#PS_Export.concat('CourseIdentifier','subject','number','Title','Credits','Type')
-
-'''CourseIdentifier	Subject	Number	Title	Credits	OrgUnitIdentifier	Type	Description	CipCode'''
+'''CourseIdentifier	Subject	Number	Title	Credits	OrgUnitIdentifier	Type	Description	CipCode
 '''
 '''
 'CourseIdentifier', 'Subject', 'Number', 'Title', 'Credits', 'OrgUnitIdentifier', 'Type', 'Description', 'CipCode'
 '''
 
-'SectionIdentifier', 'TermIdentifier', 'CourseIdentifier', 'Subject', 'CourseNumber', 'Number', 'BeginDate', 'EndDate', 'OrgUnitIdentifier', 'Title', 'Credits', 'DeliveryMode', 'Location', 'Description', 'CrossListingIdentifier'
-4226CIT105A0Y3	4226	CIT105	CIT	 105	A0Y3	8/15/2022	12/8/2022		Introduction to Computers	3.00	HYBRID	OWC		
-'''
+Course_Export = CLAPI.CLData()
 
-organizational_units = ['OrgUnitIdentifier', 'Name', 'Acronym', 'ParentIdentifier', 'Type']
-academic_term = ['TermIdentifier', 'Name', 'BeginDate', 'EndDate', 'ParentIdentifier', 'Type']
-courses_ = ['CourseIdentifier', 'Subject', 'Number', 'Title', 'Credits', 'OrgUnitIdentifier', 'Type', 'Description', 'CIPCode']
-sections = ['SectionIdentifier', 'TermIdentifier', 'CourseIdentifier', 'Subject', 'CourseNumber', 'Number', 'BeginDate', 'EndDate', 'OrgUnitIdentifier', 'Title', 'Credits', 'DeliveryMode', 'Location', 'Description', 'CrossListingIdentifier']
-instructors = ['PersonIdentifier', 'SectionIdentifier', 'FirstName', 'LastName', 'Email', 'Role']
+organizational_units = ['OrgUnitIdentifier','Name', 'Acronym', 'ParentIdentifier', 'Type']
+academic_term = ['TermIdentifier', 'Name','BeginDate', 'EndDate', 'ParentIdentifier', 'Type']
+courses = ['CourseIdentifier', 'Subject', 'Number', 'Title','Credits', 'OrgUnitIdentifier', 'Type', 'Description', 'CIPCode']
+sections = ['SectionIdentifier', 'TermIdentifier', 'CourseIdentifier', 'Subject', 'CourseNumber', 'Number', 'BeginDate','EndDate', 'OrgUnitIdentifier', 'Title', 'Credits', 'DeliveryMode', 'Location', 'Description', 'CrossListingIdentifier']
+instructors = ['PersonIdentifier', 'SectionIdentifier','FirstName', 'LastName', 'Email', 'Role']
